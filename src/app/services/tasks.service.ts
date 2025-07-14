ebab11d78dcf73b67a19c923375a39304309ccb4
Datn7 <datunashvili72@gmail.com>
2025-07-15 00:34:24 +0400

    configured connection to api

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { signal } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
   tasks = signal<string[]>([]);
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   loadTasks() {
     this.http
-      .get<string[]>('https://localhost:port/api/tasks')
+      .get<string[]>(`${this.baseUrl}/tasks`)
       .subscribe((data) => this.tasks.set(data));
   }
 
   addTask(task: string) {
     this.http
-      .post('https://localhost:port/api/tasks', task)
+      .post(`${this.baseUrl}/tasks`, task)
       .subscribe(() => this.loadTasks());
   }
 }
