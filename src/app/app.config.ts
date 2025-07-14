7dbd8a92acc00db1396fe1ce6b56493e74714417
Datn7 <datunashvili72@gmail.com>
2025-07-15 00:54:03 +0400

    generated navigation and landing component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { FormsModule } from '@angular/forms';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom(FormsModule),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './features/tasks/tasks.component';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [{ path: '', component: TasksComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tasks', component: TasksComponent },
+];
