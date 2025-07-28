939c0b73b9d8e4edc478e39c1669e97296a9cf86
Datn7 <datunashvili72@gmail.com>
2025-07-28 13:02:03 +0400

    db tasks routing and menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './features/tasks/tasks.component';
 import { HomeComponent } from './features/home/home.component';
+import { DbTasksComponent } from './features/db-tasks/db-tasks.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'tasks', component: TasksComponent },
+  { path: 'db-tasks', component: DbTasksComponent },
 ];
